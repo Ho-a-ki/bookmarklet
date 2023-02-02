7510fb008d0ba0a2f72843b20047361e352a5047
gh-surface <gh4777@naver.com>
2023-02-02 15:03:11 +0900

    user js 수정

--- user.js
+++ user.js
@@ -3,9 +3,21 @@ javascript: function add_script(){
     var jScript;
     jScript = document.createElement('script');
     jScript.language = 'javascript';
-    jScript.src = 'https://cdn.jsdelivr.net/gh/ho-a-ki/bookmarklet@main/gwp.js';
+    jScript.src = 'https://cdn.jsdelivr.net/gh/ho-a-ki/bookmarklet@main/main.js';
     jScript.type = 'text/javascript';
     head.appendChild(jScript);
  } 
  
- add_script();
+ try {
+    add_script();
+    let sign;
+ } catch (error) {
+    console.error(error);
+ }
+ 
+ sign = prompt("어떤 기능을 실행하겠습니까? \n 1. 증정품 체크 \n 2. 이카운트 입력");
+ if (sign == "1") {
+    returnSumByName();
+    } else if (sign == "2" ) {
+    naverClipboardEC();
+}
